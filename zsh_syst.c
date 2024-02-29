cdde4e9fbe92757c85a0b67cb6c21f56239dc0be
Heirloom <46321493+CHARAIESTPRO@users.noreply.github.com>
2024-02-29 19:22:45 +0100

    Update zsh_syst.c

--- zsh_syst.c
+++ zsh_syst.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/select.h>
 
 #define GREEN "\x1B[32m"
 #define RESET "\x1B[0m"
@@ -11,21 +12,42 @@ void starter() {
     char pass[6];
     printf("Enter password: ");
     fflush(stdout);
-    if (fgets(pass, sizeof(pass), stdin) == NULL) {
-        exit(42);
+    
+    // Set up a file descriptor set for stdin
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds);
+    
+    // Set timeout to 3 seconds
+    struct timeval timeout;
+    timeout.tv_sec = 3;
+    timeout.tv_usec = 0;
+    
+    int ret = select(STDIN_FILENO + 1, &fds, NULL, NULL, &timeout);
+    if (ret == -1) {
+        perror("select");
+        exit(EXIT_FAILURE);
+    } else if (ret == 0) {
+        // Timeout occurred
+        printf("Timeout occurred\n");
+        exit(EXIT_FAILURE);
     } else {
-        pass[strcspn(pass, "\n")] = 0; // Remove trailing newline
-        if (strcmp(pass, "60065") == 0) {
-            printf("%sWelcome back king%s\n", GREEN, RESET);
-            exit(13);
+        if (fgets(pass, sizeof(pass), stdin) == NULL) {
+            exit(EXIT_FAILURE);
         } else {
-            exit(42);
+            pass[strcspn(pass, "\n")] = 0; // Remove trailing newline
+            if (strcmp(pass, "60065") == 0) {
+                printf("%sWelcome back king%s\n", GREEN, RESET);
+                exit(EXIT_SUCCESS);
+            } else {
+                exit(EXIT_FAILURE);
+            }
         }
     }
 }
 
 void no_ctrlc() {
-    exit(42);
+    exit(EXIT_FAILURE);
 }
 
 int main() {
@@ -34,5 +56,5 @@ int main() {
     while (1) {
         starter();
     }
-    return 42;
+    return EXIT_FAILURE;
 }
